59374b16088ae44ba6ed2e0e020f6d02d9419e7c
= <=>
2010-07-31 09:51:14 -0700

    started adding codes

--- functions.php
+++ functions.php
@@ -1,8 +1,30 @@
 <?php
 
 include_once('alphaID.php');
-$config = parse_ini_file('config.ini');
+$config = parse_ini_file('config.ini', true);
 
+$functions = array('new_url', 'fetch_url');
 
+if (in_array($_REQUEST['func'], $functions))
+	$_REQUEST['func']();
+
+function new_url() {
+	global $config;
+
+	$dbh = new PDO('dblib:host=' . $config['coolsitebro']['host'] . ';dbname=coolsitebro',
+		$config['coolsitebro']['username'], $config['coolsitebro']['password']);
+
+	$url = $_REQUEST['url'];
+	$private = $_REQUEST['private'];
+	
+	$attributes = array('private'=>$_REQUEST['private'], 'count'=>0);
+	
+	$sth = $dbh->prepare("insert into redirects (url, attributes) values (?, ?)");
+	$sth->execute(array($url, $attributes));
+
+}
+
+function fetch_url() {
+}
 
 ?>
