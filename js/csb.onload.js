78817ce5b2656be318e895808c3fb26b082c6b15
Renato Untalan Jr <runtalan@remy.local>
2010-08-02 23:18:49 -0700

    (1)  Added ajax preloader gif (2)  Created "shortenController" js file to handle the ajax and dom manipulation when submitting links for shortening. (3)  modified vanilla.html with new DOM elements to show preloader, and show result on success.

--- js/csb.onload.js
+++ js/csb.onload.js
@@ -3,16 +3,25 @@ $(document).ready(function(){
 		$("div.footer").fadeIn("slow");
 		$(this).unbind();
 	})
+	
+	// focus url bar on load
 	$("#url").focus();
+	
+	// konami code leetness
 	$(window).konami(function(){
 		alert("i am the eggman");
 	});
+	
+	// set up scrollable
 	$(".scrollable").scrollable();
 	var instance = $(".scrollable").data("scrollable");
+	
 	// bind menus
 	$("#about").click(function(){instance.seekTo(1)})
 	$("#api").click(function(){instance.seekTo(2)})
 	$("#contact").click(function(){instance.seekTo(3)})
 	$("#stats").click(function(){instance.seekTo(4)})
 
+	shortenController.init(); 
+	
 })
